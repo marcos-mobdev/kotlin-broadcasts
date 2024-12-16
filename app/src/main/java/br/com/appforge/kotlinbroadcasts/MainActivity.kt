cb3cb711de1af1b83a0e75aa7c2d714ce3311410
Marcos Pinheiro <marcospinheiro.em@gmail.com>
2024-12-15 22:11:09 -0300

    added wifi status check

--- app/src/main/java/br/com/appforge/kotlinbroadcasts/MainActivity.kt
+++ app/src/main/java/br/com/appforge/kotlinbroadcasts/MainActivity.kt
@@ -1,7 +1,10 @@
 package br.com.appforge.kotlinbroadcasts
 
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.appforge.kotlinbroadcasts.databinding.ActivityMainBinding
@@ -20,6 +23,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initializeBroadcast()
+        checkWifiStatus()
+    }
+
+    private fun checkWifiStatus(){
+        val wifiManager = getSystemService(WifiManager::class.java)
+        //Alternative:
+        //val wifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+        if(wifiManager.isWifiEnabled){
+            binding.textView.text = "Wi-Fi Enabled"
+        }else{
+            binding.textView.text = "Wi-Fi Disabled"
+        }
     }
 
     private fun initializeBroadcast(){
@@ -29,6 +44,7 @@ class MainActivity : AppCompatActivity() {
             addAction(Intent.ACTION_BATTERY_LOW)
             addAction(Intent.ACTION_POWER_CONNECTED)
             addAction(Intent.ACTION_POWER_DISCONNECTED)
+            addAction(WifiManager.WIFI_STATE_CHANGED_ACTION)
         }.also { intentFilter ->
             registerReceiver(myBroadcastReceiver, intentFilter)
         }
--- app/src/main/java/br/com/appforge/kotlinbroadcasts/MyBroadcastReceiver.kt
+++ app/src/main/java/br/com/appforge/kotlinbroadcasts/MyBroadcastReceiver.kt
@@ -3,19 +3,45 @@ package br.com.appforge.kotlinbroadcasts
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.wifi.WifiManager
 import android.util.Log
 import android.widget.Toast
 
 class MyBroadcastReceiver : BroadcastReceiver() {
 
+
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        val message = when(intent.action){
+
+        val wifiStatus = intent.getIntExtra(
+            WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN
+        )
+
+        val wifiStatusMessage = when(wifiStatus){
+            WifiManager.WIFI_STATE_ENABLED-> "Wi-Fi Enabled"
+            WifiManager.WIFI_STATE_DISABLED-> "Wi-Fi Disabled"
+            WifiManager.WIFI_STATE_ENABLING-> "Wi-Fi Enabling"
+            WifiManager.WIFI_STATE_DISABLING-> "Wi-Fi Disabling"
+            else -> ""
+        }
+
+        val batteryStatusMessage = when(intent.action){
             Intent.ACTION_BATTERY_LOW -> "ACTION_BATTERY_LOW"
             Intent.ACTION_POWER_CONNECTED -> "ACTION_POWER_CONNECTED"
             Intent.ACTION_POWER_DISCONNECTED -> "ACTION_POWER_DISCONNECTED"
-            else-> "UNKNOWN ACTION"
+            //WifiManager.WIFI_STATE_CHANGED_ACTION -> "WIFI_STATE_CHANGED_ACTION" //For
+            else-> ""
         }
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+
+        Log.i("info_broadcast", "wifi: $wifiStatusMessage")
+        Log.i("info_broadcast", "battery: $batteryStatusMessage")
+
+        if (batteryStatusMessage.isNotEmpty()) {
+            Toast.makeText(context, batteryStatusMessage, Toast.LENGTH_SHORT).show()
+        }
+        if(wifiStatusMessage.isNotEmpty()){
+            Toast.makeText(context, wifiStatusMessage, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
